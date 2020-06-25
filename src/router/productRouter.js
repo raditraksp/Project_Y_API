bf274415e35925968ea1ee4b8126861ba1260c05
raditraksp <raditraksatria@gmail.com>
2020-06-25 17:27:40 +0700

    ADD, READ, DELETE PRODUCT DONE

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -36,12 +36,11 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
     try {
         // {name, description, stock, price} = req.body
         // {picture} = req.file
-        const sqlInsert = `INSERT INTO table_products SET product="${req.body.product}", user_id=${req.user.id}, category_id=${req.body.category_id}, product_detail="${req.body.product_detail}";`
-        const sql2= `INSERT INTO table_product_value SET price_basic = ${req.body.price_basic}, price_premium=${req.body.price_premium}, detail_basic="${req.body.detail_basic}", detail_premium="${req.body.detail_premium}"`
-        // const dataInsert = {...req.body, user_id : req.user.id}
+        const sqlInsert = `INSERT INTO table_products SET ?`
+        const dataInsert = {...req.body, user_id : req.user.id}
 
         // insert semua data text
-        conn.query(sqlInsert, sql2, async (err, result) => {
+        conn.query(sqlInsert, dataInsert, async (err, result) => {
             if (err) return res.status(500).send(err)
             
             // Generate file name
@@ -57,7 +56,6 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
                 if (err) return res.status(500).send(err)
 
                 res.status(200).send({message: "Insert data berhasil"})
-
             })
          
 
@@ -69,13 +67,7 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
 
 // READ ALL PRODUCTS
 router.get('/products', auth, (req, res) => {
-    const sqlSelect = `
-        SELECT 
-            du.id, du.username, du.avatar,
-            p.id, p.name , p.product_photo, p.product_detail, p.price
-        FROM table_detail_users du
-        JOIN table_products p ON du.id = p.user_id WHERE u.did != ${req.user.id}
-    `
+    const sqlSelect = `SELECT * FROM table_products WHERE user_id != ${req.user.id}`
 
     conn.query(sqlSelect, (err, result) => {
         if(err) return res.status(500).send(err)
@@ -86,7 +78,7 @@ router.get('/products', auth, (req, res) => {
 
 // READ OWN PRODUCTS
 router.get('/products/me', auth, (req, res) => {
-    const sqlSelect = `SELECT * FROM products WHERE user_id = ${req.user.id}`
+    const sqlSelect = `SELECT * FROM table_products WHERE user_id = ${req.user.id}`
 
     conn.query(sqlSelect, (err, result) => {
         if(err) return res.status(500).send(err)
@@ -115,7 +107,7 @@ router.get('/product/picture/:fileName', (req, res) => {
 
 // READ DETAIL PRODUCT
 router.get('/product/:id', auth, (req, res) => {
-    const sqlSelect = `SELECT * FROM products WHERE id = ${req.params.id}`
+    const sqlSelect = `SELECT * FROM table_products WHERE id = ${req.params.id}`
     conn.query(sqlSelect, (err, result) => {
         if(err) return res.status(500).send(err)
         
@@ -123,6 +115,15 @@ router.get('/product/:id', auth, (req, res) => {
     })
 })
 
+// DELETE PRODUCT
+router.delete('/product/:product_id', auth, (req, res) => {
+    const sql = `DELETE FROM table_products WHERE id = ${req.params.product_id}`
+    conn.query(sql, (err, result) => {
+        if(err) return res.status(500).send(err)
+        
+        res.status(200).send({message:"Delete Success"})
+    })
+})
 
 
 
