2e4e12b0891d7db897235b42a92a91633cc89c14
raditraksp <raditraksatria@gmail.com>
2020-06-25 16:55:28 +0700

    progress add table_product_value

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -36,17 +36,18 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
     try {
         // {name, description, stock, price} = req.body
         // {picture} = req.file
-        const sqlInsert = `INSERT INTO table_products SET ?`
-        const dataInsert = {...req.body, user_id : req.user.id}
+        const sqlInsert = `INSERT INTO table_products SET product="${req.body.product}", user_id=${req.user.id}, category_id=${req.body.category_id}, product_detail="${req.body.product_detail}";`
+        const sql2= `INSERT INTO table_product_value SET price_basic = ${req.body.price_basic}, price_premium=${req.body.price_premium}, detail_basic="${req.body.detail_basic}", detail_premium="${req.body.detail_premium}"`
+        // const dataInsert = {...req.body, user_id : req.user.id}
 
         // insert semua data text
-        conn.query(sqlInsert, dataInsert, async (err, result) => {
+        conn.query(sqlInsert, sql2, async (err, result) => {
             if (err) return res.status(500).send(err)
             
             // Generate file name
             const fileName = `${shortid.generate()}.png`
             // Simpan gambar
-            await sharp(req.file.buffer).resize(300).png().toFile(`${productsDirectory}/${fileName}`)
+            await sharp(req.file.buffer).resize(500).png().toFile(`${productsDirectory}/${fileName}`)
             
             const sqlUpdate = `UPDATE table_products SET product_photo = ? WHERE id = ?`
             const dataUpdate = [fileName, result.insertId]
@@ -56,6 +57,7 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
                 if (err) return res.status(500).send(err)
 
                 res.status(200).send({message: "Insert data berhasil"})
+
             })
          
 
@@ -69,10 +71,10 @@ router.post('/products', auth, product.single("product_photo"),  (req, res) => {
 router.get('/products', auth, (req, res) => {
     const sqlSelect = `
         SELECT 
-            u.id 'usrId' , u.name 'usrName', u.avatar 'usrAvatar',
-            p.id, p.name , p.picture, p.description, p.stock, p.price
-        FROM users u
-        JOIN products p ON u.id = p.user_id WHERE u.id != ${req.user.id}
+            du.id, du.username, du.avatar,
+            p.id, p.name , p.product_photo, p.product_detail, p.price
+        FROM table_detail_users du
+        JOIN table_products p ON du.id = p.user_id WHERE u.did != ${req.user.id}
     `
 
     conn.query(sqlSelect, (err, result) => {
