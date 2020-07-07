b5b64d7cd99f689305f89651e0634cd364a1adb6
sawm97 <sriariefwibawamukti@gmail.com>
2020-07-07 10:10:12 +0700

    transactionRoute

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const port = 2022
 
 const userRouter = require('./src/router/userRouter')
 const productRouter = require('./src/router/productRouter')
+const transactionRouter = require('.src/router/transactionRouter')
 
 app.use(cors())
 app.use(express.json())
 
 app.use(userRouter)
 app.use(productRouter)
+app.use(transactionRouter)
 
 
 app.get('/', (req, res) => {
